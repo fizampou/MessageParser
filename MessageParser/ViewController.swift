7165f7449532df00f942fc8a2fff5afdd64857d7
Φίλιππος Ζαμπούνης <zaabalonso@gmail.com>
2016-04-28 20:40:47 +0200

    Initial Commit

---
+++ MessageParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessageParser
+//
+//  Created by Filippos Zampounis on 28/04/16.
+//  Copyright © 2016 Filippos Zampounis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
